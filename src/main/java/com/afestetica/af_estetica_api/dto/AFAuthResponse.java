ff40020e3017644ec6c2de2e59948aa932f366ed
Vitor <vytroxalberto@gmail.com>
2025-08-03 21:56:04 -0300

    feat(auth): criar endpoints de registro e login com DTOs

---
+++ src/main/java/com/afestetica/af_estetica_api/dto/AFAuthResponse.java
@@ -0,0 +1,13 @@
+package com.afestetica.af_estetica_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AFAuthResponse {
+
+    private String token;
+}
---
+++ src/main/java/com/afestetica/af_estetica_api/dto/AFLoginRequest.java
@@ -0,0 +1,14 @@
+package com.afestetica.af_estetica_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AFLoginRequest {
+
+    private String email;
+    private String password;
+}
