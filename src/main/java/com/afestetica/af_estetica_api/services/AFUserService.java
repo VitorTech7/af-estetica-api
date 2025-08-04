2050a15255f818d43040fc348c189983bfd6b3bf
Vitor <vytroxalberto@gmail.com>
2025-08-03 22:03:26 -0300

    Adicionado a classe config

--- src/main/java/com/afestetica/af_estetica_api/services/AFUserService.java
+++ src/main/java/com/afestetica/af_estetica_api/services/AFUserService.java
@@ -3,7 +3,6 @@ package com.afestetica.af_estetica_api.services;
 import com.afestetica.af_estetica_api.model.AFUserModel;
 import com.afestetica.af_estetica_api.repositories.AFUserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,12 +12,6 @@ import java.util.Optional;
 public class AFUserService {
 
     private final AFUserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    public AFUserModel register(AFUserModel user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
-    }
 
     public Optional<AFUserModel> findByEmail(String email) {
         return userRepository.findByEmail(email);
